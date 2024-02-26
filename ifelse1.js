1909ed7dba9e507fb63d4e272802d6c7fa921dbe
ana.camacho <129320167+anacamacho-cloud@users.noreply.github.com>
2024-02-26 12:06:19 -0300

    Update ifelse1.js

--- ifelse1.js
+++ ifelse1.js
@@ -1,13 +1,19 @@
+// a função que o "verificarLogin" irá exercer
 function verificarLogin(){
+    //let é uma variavel que quando executada não ficará na memoria, o proximo é para chamar elementos do html
     let txtLogin = document.getElementById("txtLogin");
     let txtSenha = document.getElementById("txtSenha");
 
+    //const é uma variavel 
     const login = "teste";
-    senha = "teste2";
+    let senha = "teste2";
 
+    //se a variavel "txtLogin" for igual ao "login e txtSenha" = senha
+    //.value é para o valor da variavel
     if(txtLogin.value == login && txtSenha.value == senha) {
-        console.log("Logado com sucesso!")
+        console.log("Logado com sucesso!");
     } else{
-        console.log("Login ou Senha incorretos.")
+        console.log("Login ou Senha incorretos.");
     }
 }
+
